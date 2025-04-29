e2fa59b34efa9fa21988cc53bfe88f910efa82c4
asima <asimaabdikarimoova6364@gmail.com>
2025-04-29 23:08:54 +0500

    p6

--- src/BST.java
+++ src/BST.java
@@ -1,5 +1,6 @@
 public class BST<K extends Comparable<K>, V> {
     private Node root;
+    private int size = 0;
     private class Node {
         private K key;
         private V val;
@@ -10,11 +11,110 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-    public void put(K key, V val) {}
+    public int size() {
+        return size;
+    }
 
-    public V get(K key) {}
+    public void put(K key, V val) {
+        Node newNode = new Node(key, val);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
 
-    public void delete(K key) {}
+        while (current != null) {
+            parent = current;
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) {
+                current = current.left;
+            } else if (cmp > 0) {
+                current = current.right;
+            } else {
+                current.val = val;
+                return;
+            }
+        }
+        int cmp = key.compareTo(parent.key);
+        if (cmp < 0) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
+    }
 
-    public Iterable<K> iterator() {}
+    public V get(K key) {
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) {
+                current = current.left;
+            } else if (cmp > 0) {
+                current = current.right;
+            } else {
+                return current.val;
+            }
+        }
+        return null;
+    }
+
+    public void delete(K key) {
+        Node parent = null;
+        Node current = root;
+
+        while (current != null && !current.key.equals(key)) {
+            parent = current;
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        if (current == null) {
+            return;
+        }
+        if (current.left == null || current.right == null) {
+            Node replacement = (current.left != null) ? current.left : current.right;
+
+            if (parent == null) {
+                root = replacement;
+            } else if (parent.left == current) {
+                parent.left = replacement;
+            } else {
+                parent.right = replacement;
+            }
+        }
+        else {
+            Node successorParent = current;
+            Node successor = current.right;
+
+            while (successor.left != null) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+
+            if (successorParent != current) {
+                successorParent.left = successor.right;
+                successor.right = current.right;
+            }
+
+            successor.left = current.left;
+
+            if (parent == null) {
+                root = successor;
+            } else if (parent.left == current) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+        }
+    }
+
+    public Iterable<K> iterator() {
+        return inOrderTraversal();
+    }
+
+
 }
